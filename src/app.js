52a8e1ca85e8baf91547cf8fcc5a18bb2bc12088
adelinel <adeline.latruwe@gmail.com>
2021-01-19 14:27:25 -0800

    added POST and DELETE endpoints

--- src/app.js
+++ src/app.js
@@ -4,17 +4,106 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
+const { v4: uuid } = require("uuid");
 
 const app = express();
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
+app.use(express.json());
 app.use(helmet());
 app.use(cors());
 
-app.post("/", (req, res) => {
-  res.send("POST request received.");
+// Declare an array to store the users
+const users = [
+  {
+    id: "3c8da4d5-1597-46e7-baa1-e402aed70d80",
+    username: "sallyStudent",
+    password: "c00d1ng1sc00l",
+    favoriteClub: "Cache Valley Stone Society",
+    newsLetter: "true",
+  },
+  {
+    id: "ce20079c-2326-4f17-8ac4-f617bfd28b7f",
+    username: "johnBlocton",
+    password: "veryg00dpassw0rd",
+    favoriteClub: "Salt City Curling Club",
+    newsLetter: "false",
+  },
+];
+
+app.post("/user", (req, res) => {
+  // get the data
+  const { username, password, favoriteClub, newsLetter = false } = req.body;
+
+  // validation code here
+  // if no value entered that is required
+  if (!username) {
+    return res.status(400).send("Username required");
+  }
+  if (!password) {
+    return res.status(400).send("Password required");
+  }
+  if (!favoriteClub) {
+    return res.status(400).send("favorite Club required");
+  }
+  // check type of the value and format if needed
+  if (username.length < 6 || username.length > 20) {
+    return res.status(400).send("Username must be between 6 and 20 characters");
+  }
+  // password length
+  if (password.length < 8 || password.length > 36) {
+    return res.status(400).send("Password must be between 8 and 36 characters");
+  }
+  // password contains digit, using a regex here
+  if (!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)) {
+    return res.status(400).send("Password must be contain at least one digit");
+  }
+  const clubs = [
+    "Cache Valley Stone Society",
+    "Ogden Curling Club",
+    "Park City Curling Club",
+    "Salt City Curling Club",
+    "Utah Olympic Oval Curling Club",
+  ];
+  // make sure the club is valid
+  if (!clubs.includes(favoriteClub)) {
+    return res.status(400).send("Not a valid club");
+  }
+
+  const id = uuid(); // generate a unique id
+  const newUser = {
+    id,
+    username,
+    password,
+    favoriteClub,
+    newsLetter,
+  };
+
+  users.push(newUser);
+
+  // at this point all validation passed
+  res.send("All validation passed");
+});
+
+app.get("/user", (req, res) => {
+  res.json(users);
+});
+
+app.delete("/user/:userId", (req, res) => {
+  const { userId } = req.params;
+  // find index of the user in array
+  const index = users.findIndex((u) => u.id === userId);
+
+  // make sure we actually find a user with that id
+  if (index === -1) {
+    return res.status(404).send("User not found");
+  }
+  // remove user
+  users.splice(index, 1);
+
+  res.status(204).end();
 });
 
 app.use(function errorHandler(error, req, res, next) {
